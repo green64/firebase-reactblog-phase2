f02969891b083e360f551093e1907f418f59b4c1
Samantha McCall <samanthaadams.biz@gmail.com>
2019-02-02 23:26:45 -0600

    app taking shape with nav and sample projects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+
 
 class App extends Component {
   render() {
@@ -8,6 +10,10 @@ class App extends Component {
       <BrowserRouter>
       <div className="App">
       <Navbar />   
+      {/* switch allows only route to be loaded at a time */}
+      <Switch>
+        <Route path='/' component={Dashboard} />
+      </Switch>
       </div>
       </BrowserRouter>
     );
