b9d386caccf0584e7922360c12ddf80fb15a7f31
Samantha McCall <samanthaadams.biz@gmail.com>
2019-02-16 22:41:19 -0600

    adding route guards, timestamp and dynamic user data from firestore profile

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,20 +1,24 @@
-export const createProject = (project) => {
-  return (dispatch, getState, {getFirestore}) => {
+export const createProject = project => {
+  return (dispatch, getState, { getFirestore }) => {
     // make async call to database
     const firestore = getFirestore();
     //switching from hardcoded to dynamic auth first, last and id
     const profile = getState().firebase.profile;
     const authorId = getState().firebase.auth.uid;
-    firestore.collection('projects').add({
-      ...project,
-      authorFirstName: profile.firstName,
-      authorLastName: profile.lastName,
-      authorId: authorId,
-      createdAt: new Date()
-    }).then(() => {
-      dispatch({ type: 'CREATE_PROJECT_SUCCESS' });
-    }).catch(err => {
-      dispatch({ type: 'CREATE_PROJECT_ERROR' }, err);
-    });
-  }
+    firestore
+      .collection("projects")
+      .add({
+        ...project,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authorId,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT_SUCCESS" });
+      })
+      .catch(err => {
+        dispatch({ type: "CREATE_PROJECT_ERROR" }, err);
+      });
+  };
 };
